0857ccf7096d25ee9cdd038dc936dd33139b7492
Ashish Ingle <ashish.ingle@globant.com>
2018-03-30 18:26:26 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const restService = express();
+
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+restService.use(bodyParser.json());
+
+restService.post("/math", function(req, res) {
+  var speech =
+    req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.firstNumber &&
+    req.body.result.parameters.secondNumber
+      ? (req.body.result.parameters.firstNumber + req.body.result.parameters.secondNumber)
+      : "Seems like some problem. Speak again.";
+  return res.json({
+    speech: speech,
+    displayText: speech,
+    source: "webhook-mathbot-nodejs-sample"
+  });
+});
+
+restService.listen(process.env.PORT || 3000, function() {
+    console.log("Server up and listening");
+});
